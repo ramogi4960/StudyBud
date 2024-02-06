4f0fc0b4831875cbaaaad756bdd5aaf54f34785d
Kevin <kramzyvine@icloud.com>
2024-02-06 16:38:26 +0300

    created and migrated models

--- Chatrooms/room/models.py
+++ Chatrooms/room/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=200)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    details = models.TextField(blank=True, null=True)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-updated", "-created"]
+
+    def __str__(self):
+        return self.name
+
+
+class Comments(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=True, null=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content[:50]
